c3738b5e469b8297345bd4d1dc4e8f1fa44280c3
tekkyboy <kparayao@gmail.com>
2022-02-24 00:13:57 -0800

    Kylan update

---
+++ myapp/kylan.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// User declaration in Javascript on the NodeJS framework.
+app.get('/', (req, res) => {
+  res.send('My name is Kylan!');
+})
+
+app.get('/demo', (req, res) => {
+    res.send('This is a Nodejs demo by the Beeroscope team.');
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
