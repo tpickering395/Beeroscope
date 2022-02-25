4789b94dd68f6a32db934fec541ddb39e9e41859
Thomas Pickering <tpickering@cpp.edu>
2022-02-24 20:34:09 -0800

    Added my page and HTTP GET call

--- myapp/app.js
+++ myapp/app.js
@@ -2,15 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// Hello world program in Javascript on the NodeJS framework.
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+})
+
+app.get('/thomas', (req, res) => {
+  res.send('Hello! My name is Thomas Pickering. I am a member of the Beeroscope team.');
 })
 
 app.get('/demo', (req, res) => {
     res.send('This is a Nodejs demo by the Beeroscope team.');
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
