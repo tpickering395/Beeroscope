572d6630c6d3411c56af48ffa206f0aa09b90f56
tekkyboy <kparayao@gmail.com>
2022-02-28 18:49:26 -0800

    updated moment

--- myapp/datetime.js
+++ myapp/datetime.js
@@ -4,6 +4,7 @@ require.config({
     },
  });
  require(['moment'], function (moment) {
+    moment.locale('fr');
     var a = moment().format("LLLL");
     document.getElementById("datedisplay").innerHTML = a;
  });
