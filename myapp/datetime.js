c5103c23ba10aa91cd62f7613c48e736bd8767e9
tekkyboy <kparayao@gmail.com>
2022-02-28 19:34:56 -0800

    deleted datetime.js, updated datetime.html

--- myapp/datetime.js
+++
@@ -1,9 +0,0 @@
-require.config({
-    paths:{
-       'moment':'node_modules/moment/moment.js',
-    },
- });
- require(['moment'], function (moment) {
-    var a = moment().format("LLLL");
-    document.getElementById("datedisplay").innerHTML = a;
- });
