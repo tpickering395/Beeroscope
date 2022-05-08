b0bb3c5f65ef5b7ff1b0df82262da1e4e689521d
Thomas Pickering <tpickering@cpp.edu>
2022-05-08 03:44:59 -0700

    Added detailed beer report when clicking on a beer entry in Search results

--- client/src/Table.js
+++ client/src/Table.js
@@ -7,10 +7,16 @@ import './Table.css'
 export default class Table extends React.Component {
  
     constructor(props){
-    super(props);
-    this.getHeader = this.getHeader.bind(this);
-    this.getRowsData = this.getRowsData.bind(this);
-    this.getKeys = this.getKeys.bind(this);
+        super(props);
+        this.getHeader = this.getHeader.bind(this);
+        this.getRowsData = this.getRowsData.bind(this);
+        this.getKeys = this.getKeys.bind(this);
+        this.getDetailedData = this.getDetailedData.bind(this);
+        this.getFoodPairing = this.getFoodPairing.bind(this);
+        this.getMethod = this.getMethod.bind(this);
+        this.getIngredients = this.getIngredients.bind(this);
+        this.getHops = this.getHops.bind(this)
+        this.getMalts = this.getMalts.bind(this)
     }
     
     // Gets the keys of the data, in this context it means to get the data categories. The keys we obtain are the first the which correspond to:
@@ -18,9 +24,9 @@ export default class Table extends React.Component {
     // An improvement would be to make this dynamic and dependent on user choice, there are about 30 other variables to choose from in the Punk API.
     getKeys = function(){
         var temp = Object.keys(this.props.data[0]);
-        console.log(this.props.data[0])
+      //  console.log(this.props.data[0])
         var keys = Object.keys(this.props.data[0]).slice(0, 3);
-        console.log(`Truth test: ${this.props.tags[0]} `);
+      //  console.log(`Truth test: ${this.props.tags[0]} `);
         if(this.props.tags[0]) {
             keys.push(temp[6])
         }
@@ -42,11 +48,11 @@ export default class Table extends React.Component {
     // Get table headers using keys from the key-value pairs.
     getHeader = function(){
         var keys = this.getKeys();
-        console.log("~~~~~KEY LOGGING~~~~~~")
+      //  console.log("~~~~~KEY LOGGING~~~~~~")
         return keys.map((key, index)=>{
             var headtitle = key;
             headtitle = headtitle.replace(/_+/g, ' ');
-            console.log(headtitle);
+       //     console.log(headtitle);
             return <th key={key}>{headtitle.toUpperCase()}</th>
         })
     }
@@ -55,26 +61,156 @@ export default class Table extends React.Component {
     getRowsData = function(){
         var items = this.props.data;
         var keys = this.getKeys();
-        console.log("~~~~~INDEX LOGGING~~~~~~")
+      //  console.log("~~~~~INDEX LOGGING~~~~~~")
         return items.map((row, index)=>{
            
-            console.log(index);
-            return <tr key={index}><RenderRow key={index} data={row} keys={keys}/></tr>
+       //      console.log(index);
+       //     console.log(items[index]);
+            return <tr key={index} onClick={() => this.getDetailedData(items[index])}><RenderRow key={index} data={row} keys={keys}/></tr>
         })
     }
+
+    getFoodPairing = (beer) => {
+        var result = "";
+        var count = 0
+        beer["food_pairing"].map((index) => {
+            if(count > 0) {
+                result += ", or " + index;
+            }
+            else{ 
+                count++;
+                result += index; 
+            }
+        });
+     //   console.log(result);
+        return result;
+    }
+
+    getMethod = (method) => {
+        var ferment = "Ferments at: " + method["fermentation"]["temp"]["value"] + " " + method["fermentation"]["temp"]["unit"];
+        var twist = "Twist: " + (typeof method["fermentation"]["twist"] === 'undefined' ? "N/A" : method["fermentation"]["twist"]);
+        var mash_temp = "Mash temperature: " + method["mash_temp"]["0"]["temp"]["value"] + " " +   method["mash_temp"]["0"]["temp"]["unit"];
+
+        var result = mash_temp + " <br/> " + (twist.length < 10 ? " " : twist) + " <br/> " + ferment + " <br/> ";
+        return result;
+    }
+
+    getMalts = (malts) => {
+        var result = "Malts: <br/>";
+        malts.map((item, index) => {
+            if(index > 0) {
+                result += "<br/>- " + item["amount"]["value"] + " " + item["amount"]["unit"] + " of " + item["name"];
+            }
+            else {result += "<br/>- " + item["amount"]["value"] + " " + item["amount"]["unit"] + " of " + item["name"];}
+            
+        });
+        result += ""
+        return result;
+    }
+
+    getHops = (hops) => {
+        var result = "\tHops: <br/>";
+        var start = "";
+        var middle = "";
+        var end = "";
+        var other = "";
+        hops.map((item, index) => {
+            if(item["add"] === "start") {
+                start += "<br/>- " + item["amount"]["value"] + " " + item["amount"]["unit"] + " of " + item["name"] + ". Used for " + item["attribute"];
+            }
+            else if (item["add"] === "middle"){
+                middle += "<br/>- " + item["amount"]["value"] + " " + item["amount"]["unit"] + " of " + item["name"] + ". Used for " + item["attribute"];
+            }
+            else if (item["add"] === "end"){
+                end += "<br/>- " + item["amount"]["value"] + " " + item["amount"]["unit"] + " of " + item["name"] + ". Used for " + item["attribute"];
+            }
+            else {
+                other += "<br/>- " + item["amount"]["value"] + " " + item["amount"]["unit"] + " of " + item["name"] + ". Used for " + item["attribute"];
+            }
+        });
+        start += "<br/>**Used at the start.";
+        middle += "<br/>**Used in the middle.";
+        end += "<br/>**Used at the end.";
+        other += "<br/>";
+        result += start + "<br/>" + middle + "<br/>" + end + " <br/> " + other + " <br/> ";
+        return result;
+    }
+
+    getIngredients = (beer) => {
+        
+        var hops = this.getHops(beer["ingredients"]["hops"]);
+        var malts = this.getMalts(beer["ingredients"]["malt"]);
+        var yeast = beer["ingredients"]["yeast"];
+        var result = this.getHops(beer["ingredients"]["hops"]) + this.getMalts(beer["ingredients"]["malt"]) + " <br/> " + " <br/> " + "Yeast: " + beer["ingredients"]["yeast"];
+        return result;
+    }
+
+    getDetailedData = function(beer){
+        console.log(beer);
+
+        var parent =  document.getElementById('Detail-section');
+        document.getElementById('title_header').hidden = false;
+
+
+        const beerName = beer["name"];
+        document.getElementById('beer_name').innerHTML = beerName + "<br/>";
+
+        const img_url = beer["image_url"];
+        document.getElementById('beer_image').innerHTML = "<img src=" + img_url + " width=100px height=500px/>";
+
+        const desc_info = beer["description"];
+        document.getElementById('description').innerHTML = "Description: " + desc_info;
+
+        const ph_level = beer["ph"];
+        document.getElementById('ph').innerHTML = "<p> pH level: " + ph_level + " </p>";
+
+        const brewery_tips = beer["brewers_tips"];
+        document.getElementById('brewerytips').innerHTML = "Brewery tips: " + brewery_tips;
+
+        const volume = "Volume: " +  beer["volume"]["value"] + " " + beer["volume"]["unit"];
+        document.getElementById('volume').innerHTML = "<br /><p>" + volume + "</p>";
+
+        const ingredients = "<br/> " + this.getIngredients(beer);
+        document.getElementById('ingredient-list').innerHTML = ingredients;
+
+        const method = this.getMethod(beer["method"]);
+        document.getElementById('mash').innerHTML = "<br/> " + method + " <br/> ";
+
+        const food_pairing = this.getFoodPairing(beer);
+        document.getElementById('foodpair').innerHTML = "<br /><p> Food Pairing: " + food_pairing + " </p>";
+    }
+
     
     // Returns the Table in HTML form.
     render() {
+        
         return (
-            <div className="table-wrapper">
-            <table className="fl-table">
-            <thead>
-            <tr>{this.getHeader()}</tr>
-            </thead>
-            <tbody>
-            {this.getRowsData()}
-            </tbody>
-            </table>
+            <div>
+                <div className="table-wrapper">
+                    <table className="fl-table">
+                    <thead>
+                    <tr>{this.getHeader()}</tr>
+                    </thead>
+                    <tbody>
+                    {this.getRowsData()}
+                    </tbody>
+                    </table>
+                </div>
+                <center>
+                <div id='Detail-section' style={{color : "#45f5f5"}} className="detailed">
+                        <h1 id='title_header' hidden={true}><p style={{color : "#FFFF00"}}>🍺 Beer Report 🍺</p></h1>
+                        <h2 id='beer_name' style={{color : "#FFFF00"}}></h2>
+                        <div id='beer_image'></div>
+                        <p id="description"></p>
+                        <div id='volume'></div>
+                        <div id='ph'></div>
+                        <p id="foodpair"></p>
+                        <p id="brewerytips"></p>
+                        <p id="ingredient-list"></p>
+                        <p id="mash"></p>
+                        
+                    </div>
+                </center>
             </div>
         
         );
@@ -82,13 +218,17 @@ export default class Table extends React.Component {
    }
    // Returns the Row's data in TableData (td) entries. 
    const RenderRow = (props) =>{
-    console.log("~~~~ROW LOGGING~~~~");
+   // console.log("~~~~ROW LOGGING~~~~");
     return props.keys.map((key, index)=>{
-        console.log(props.data[key])
-        console.log(key)
+     //   console.log("index: " + index);
+     //   console.log(props.data[key])
+     //   console.log(key)
         if(key === "image_url") {
             
             return <td key={props.data[key] + 25}><img src={props.data[key]} className="photo"/></td>
+        }
+        else if(key === "name") {
+            return <td key={props.data[key] + 25}><ul>{props.data[key]}</ul></td>
         }
         return <td key={props.data[key] + 25}><ul>{props.data[key]}</ul></td>
     })
